3facd4657fb1a2f09f55144d66f78d70568ff614
KenShinNguyen <73057902+KenShinNguyen@users.noreply.github.com>
2026-07-18 09:03:48 +0000

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -3,15 +3,13 @@
 # Định nghĩa đường dẫn tương đối trong Github Workspace
 DIR="rules"
 BLOCK_OUT="./$DIR/blocklists.txt"
-ALLOW_OUT="./$DIR/allowlists.txt"
 BLOCK_TMP="/tmp/blocklists.tmp"
-ALLOW_TMP="/tmp/allowlists.tmp"
 
 # Tạo thư mục rules nếu chưa có
 mkdir -p "./$DIR"
 
 # Cleanup khi script exit
-trap "rm -f $BLOCK_TMP $ALLOW_TMP; exit" INT TERM EXIT
+trap "rm -f $BLOCK_TMP; exit" INT TERM EXIT
 
 extract_domains() {
   awk '{
@@ -36,13 +34,7 @@ https://github.com/mullvad/dns-blocklists/blob/main/output/doh/doh_privacy.txt
 https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/pro.plus.txt \
 | extract_domains > "$BLOCK_TMP"
 
-echo "Downloading and processing allowlists..."
-curl -fsSL --max-time 60 \
-https://raw.githubusercontent.com/bibicadotnet/AdGuard-Home-blocklists/refs/heads/main/whitelist.txt \
-| extract_domains > "$ALLOW_TMP"
-
 # Di chuyển file tmp vào thư mục đích
 mv "$BLOCK_TMP" "$BLOCK_OUT"
-mv "$ALLOW_TMP" "$ALLOW_OUT"
 
-echo "Done. Files saved to $BLOCK_OUT and $ALLOW_OUT"
+echo "Done. Files saved to $BLOCK_OUT"
