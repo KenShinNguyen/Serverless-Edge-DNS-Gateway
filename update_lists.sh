07f089810182a02c64b296298d6ddc65e95fdcae
KenShinNguyen <73057902+KenShinNguyen@users.noreply.github.com>
2026-07-31 20:25:22 +0700

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -77,12 +77,18 @@ echo ""
 # Danh sách URLs (thêm/xóa URL tại đây). Có thể thêm mirror GitHub/raw.githubusercontent để dự phòng.
 declare -a URLS=(
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/pro.txt"
+  "https://raw.githubusercontent.com/mullvad/dns-blocklists/refs/heads/main/output/doh/doh_adblock.txt"
+  "https://big.oisd.nl/rpz"
   "https://v.firebog.net/hosts/AdguardDNS.txt"
+  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/popupads-onlydomains.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling-onlydomains.txt"
+  "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/gambling.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/native.winoffice.txt"
   "https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/native.xiaomi.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/native.apple.txt"
+  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/rpz/tif.mini.txt"
+  "https://urlhaus.abuse.ch/downloads/hostfile"
 )
 
 # Khởi tạo file tmp và stats
