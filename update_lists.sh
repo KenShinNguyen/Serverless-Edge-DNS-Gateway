9aa53cb6f83f2cbfd3fd7c1a94215204f4dca18a
KenShinNguyen <73057902+KenShinNguyen@users.noreply.github.com>
2026-08-02 09:48:18 +0000

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -174,8 +174,8 @@ echo ""
 
 # Danh sách URLs allowlist từ AdGuard
 declare -a ALLOW_URLS=(
-  "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SafebrowsingFilter/whitelist.txt"
-  "https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exceptions.txt"
+  "https://github.com/AdguardTeam/AdGuardSDNSFilter/blob/master/Filters/exclusions.txt.txt"
+  "https://github.com/AdguardTeam/AdGuardSDNSFilter/blob/master/Filters/exceptions.txt"
   "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/combined_whitelist.txt"
 )
 
