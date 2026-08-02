e17d5eee941e7e8fe0819864cfaae355b94cc726
KenShinNguyen <73057902+KenShinNguyen@users.noreply.github.com>
2026-08-02 09:44:22 +0000

    Add new URL to BLOCK_URLS array in update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -81,6 +81,7 @@ declare -a BLOCK_URLS=(
   "https://raw.githubusercontent.com/mullvad/dns-blocklists/refs/heads/main/output/doh/doh_adblock.txt"
   "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
   "https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers.txt"
+  "https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/threat/filter.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro-onlydomains.txt"
   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/popupads-onlydomains.txt"
   "https://raw.githubusercontent.com/mullvad/dns-blocklists/refs/heads/main/output/doh/doh_gambling.txt"
